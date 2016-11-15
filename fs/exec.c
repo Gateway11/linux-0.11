d7f4724b0effcc7e71227ab77a5f7a82a9d7ad01
daixiang789@gmail.com <daixiang789@gmail.com>
2016-11-15 20:45:58 +0800

    Compile error

--- fs/exec.c
+++ fs/exec.c
@@ -135,9 +135,9 @@ static unsigned long copy_strings(int argc,char ** argv,unsigned long *page,
 				offset = p % PAGE_SIZE;
 				if (from_kmem==2)
 					set_fs(old_fs);
-				if (!(pag = (char *) page[p/PAGE_SIZE]) &&
-				    !(pag = (char *) page[p/PAGE_SIZE] =
-				      (unsigned long *) get_free_page())) 
+				if (!(pag = (char *) page[p/PAGE_SIZE]) && 
+					!(pag = (char *) (page[p/PAGE_SIZE] = 
+					(unsigned long *) get_free_page())))
 					return 0;
 				if (from_kmem==2)
 					set_fs(new_fs);
